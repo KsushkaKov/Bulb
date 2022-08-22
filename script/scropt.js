e7914985297e615b03f782cbdcd5c5deab70ab3c
KsushkaKov <ksushka.ua@gmail.com>
2022-08-22 20:19:47 +0300

    Bulb switched on/off

--- script/scropt.js
+++ script/scropt.js
@@ -1,8 +1,18 @@
-function changeBackground(color) {
-  document.body.style.background = color;
-  
-}
 
-window.addEventListener("click", function () {
-  changeBackground("white");
-});
+
+const light = document.getElementById("light");
+light.addEventListener("click", changeColor);
+function changeColor() {
+  if (document.body.style.background == "black") {
+    document.body.style.background = "white";
+  } else {
+    document.body.style.background = "black";
+  }
+}
+// function turnOnOff() {
+//   if (light.src.match("pngwingOff")) {
+//     light.src = "img/pngwingOn.png";
+//   } else {
+//     light.src = "img/pngwingOff.png";
+//   }
+// };
