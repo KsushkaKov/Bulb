1c6108d78c2a41e7165fc2f87939dc6c474467ff
KsushkaKov <ksushka.ua@gmail.com>
2022-08-31 10:04:52 +0000

    Trying to add color picker

--- script/script.js
+++ script/script.js
@@ -1,6 +1,15 @@
+
 let btn = document.querySelector(".btn");
 let body = document.querySelector("body");
+const colorInput = document.querySelector("#color");
 
 btn.onclick = function () {
   body.classList.toggle("on");
 };
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  let color = colorInput.value;
+  document.body.style.backgroundColor = color;
+}
