2d80615fbc7a7bb4dc3e456e8a63ea57168a9bb6
Ksenia Kovalenko <ksushka.ua@gmail.com>
2022-11-14 17:37:47 +0200

    saved theme

--- script/script.js
+++ script/script.js
@@ -1,13 +1,24 @@
 let btn = document.querySelector(".btn");
 let body = document.querySelector("body");
 const colorInput = document.querySelector("#color");
+const mainColor = "#222";
 
 btn.onclick = function () {
   let isOn = body.classList.toggle("on");
   if (isOn) {
     let color = colorInput.value;
     document.body.style.backgroundColor = color;
+    localStorage.setItem("colorInput", colorInput.value);
   } else {
-    document.body.style.backgroundColor = "#222";
+    document.body.style.backgroundColor = mainColor;
+    localStorage.setItem("colorInput", mainColor);
   }
 };
+
+function checkTheme() {
+  const savedTheme = localStorage.getItem("colorInput");
+  document.body.style.backgroundColor = savedTheme;
+  colorInput.value = savedTheme;
+}
+
+checkTheme();
